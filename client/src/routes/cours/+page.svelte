66e244dbbae31a3afaf14db470c5c3c426cbb232
CELIK-Samed <samed.celik@oclock.school>
2026-04-21 13:46:07 +0200

    try first commit

---
+++ client/src/routes/cours/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    
+</script>
+
+<header>
+
+</header>
+<main class="main-content">
+    <h1 class="all-courses">Tous les cours</h1>
+
+    <div class="search">
+        <input 
+            class="search-input"
+            type="text"
+            placeholder="Rechercher un cours..."
+        />
+    </div>
+</main>
+
+<style>
+</style>
