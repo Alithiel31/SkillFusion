a22b56b5cdb49b157f1960c5349379808f9414e3
Alithiel31 <harmo.man.01@gmail.com>
2026-04-22 12:31:20 +0200

    add auth.middlewae.ts

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,4 +1,4 @@
 import { PrismaClient } from '@prisma/client';
 
 export const prisma = new PrismaClient();
-export type { User, RefreshToken } from '@prisma/client';
+export type { User, RefreshToken, Role } from '@prisma/client';
