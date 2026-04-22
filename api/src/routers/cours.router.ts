50b3d3b6167d651324babf28db808179e400e204
LoicLeger <lgr.loic@gmail.com>
2026-04-22 15:49:03 +0200

    really adding all routes for cours controller

--- api/src/routers/cours.router.ts
+++ api/src/routers/cours.router.ts
@@ -4,8 +4,11 @@ import coursController from '../controlleurs/cours.controller';
 const router= express.Router();
 
 router.get("/cours",coursController.getAll)
-
-
+router.get("/cours",coursController.getOneCours)
+router.get("/cours/homepage",coursController.getForHomePage)
+router.post("/cours",coursController.createCours)
+router.patch("/cours",coursController.updatingCours)
+router.delete("/cours",coursController.deleteCours)
 
 
 export default router;
