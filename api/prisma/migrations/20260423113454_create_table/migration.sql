426bd0ec77aeba40316167272b75c1ea96b63c4a
Adrien <devponcet07@gmail.com>
2026-04-23 13:40:37 +0200

    add category color in prisma schema and add pros in cardcours

--- api/prisma/migrations/20260423113454_create_table/migration.sql
+++ api/prisma/migrations/20260423113454_create_table/migration.sql
@@ -49,6 +49,8 @@ CREATE TABLE "categories" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT,
+    "text_color" TEXT NOT NULL,
+    "border_color" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
