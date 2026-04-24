ffb103fdef161be4b98fff7c9fd7a761d220297c
CELIK-Samed <samed.celik@oclock.school>
2026-04-24 15:18:16 +0200

    Creating router and controller for badges and test ok

---
+++ api/src/routers/badge.router.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import badgeController from '../controlleurs/badge.controller';
+
+const router = express.Router();
+
+router.get("/badges", badgeController.getAll)
+router.get("/badges/:id", badgeController.getOneBadge)
+
+router.post("/badges", badgeController.createBadge)
+
+router.patch("/badges/:id", badgeController.updatingBadge)
+
+router.delete("/badges/:id", badgeController.deleteBadge)
+
+export default router;
