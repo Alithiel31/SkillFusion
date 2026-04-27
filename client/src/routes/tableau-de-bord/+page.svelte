cff4ef704520a0856866f91989c9d69d9b35d787
LoicLeger <lgr.loic@gmail.com>
2026-04-27 10:30:22 +0200

    adding dashbard for roles

--- client/src/routes/tableau-de-bord/+page.svelte
+++ client/src/routes/tableau-de-bord/+page.svelte
@@ -1,14 +1,30 @@
-<script>
+<script lang="ts">
 	import Footer from "$lib/assets/components/Footer.svelte";
 	import Header from "$lib/assets/components/Header.svelte";
+        import AdminDashBoard from "$lib/assets/components/DashBoard/AdminDashBoard.svelte";
+        import InstructorDashBoard from "$lib/assets/components/DashBoard/InstructorDashBoard.svelte";
+        import StudentDashBoard from "$lib/assets/components/DashBoard/StudentDashBoard.svelte";
+        import {getAuth, authStore} from "$lib/services/localstorage.service.svelte"
+        import type {IUserLocalStorage} from "$lib/@types/type.localStorage"
+        import { onMount } from 'svelte';
+        let user:IUserLocalStorage|null = $state(null);
+
+        onMount(()=>{
+		getAuth()
+		user = authStore.user
+})
+
 </script>
 
 <Header/>
 <div>
-
-        <button><a href="/tableau-de-bord/admin">admin</a></button>
-        <button><a href="/tableau-de-bord/instructor">instructor</a></button>
-        <button><a href="/tableau-de-bord/student">student</a></button>
+{#if user?.role === "admin"}
+ <AdminDashBoard/>
+ {:else if user?.role === "instructor"}
+ <InstructorDashBoard/>
+ {:else if user?.role === "student"}
+ <StudentDashBoard/>
+{/if}
 
 </div>
 
