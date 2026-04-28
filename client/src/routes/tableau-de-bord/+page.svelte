301d42d9666094a6bf3584afb66a916fb2ffea51
LoicLeger <lgr.loic@gmail.com>
2026-04-28 12:04:10 +0200

    adding component app and main for dashbord

--- client/src/routes/tableau-de-bord/+page.svelte
+++ client/src/routes/tableau-de-bord/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-	import Footer from "$lib/assets/components/Footer.svelte";
+	import "../../app.css"
+        import Footer from "$lib/assets/components/Footer.svelte";
 	import Header from "$lib/assets/components/Header.svelte";
         import AdminDashBoard from "$lib/assets/components/DashBoard/AdminDashBoard.svelte";
         import InstructorDashBoard from "$lib/assets/components/DashBoard/InstructorDashBoard.svelte";
         import StudentDashBoard from "$lib/assets/components/DashBoard/StudentDashBoard.svelte";
-        import {getAuth, authStore} from "$lib/services/localstorage.service.svelte"
-        import type {IUserLocalStorage} from "$lib/@types/type.localStorage"
+        import App from "$lib/assets/components/App.svelte";
+
+        import Main from "$lib/assets/components/Main.svelte";
+        import {getAuth, authStore} from "$lib/services/localstorage.service.svelte";
+        import type {IUserLocalStorage} from "$lib/@types/type.localStorage";
         import { onMount } from 'svelte';
         let user:IUserLocalStorage|null = $state(null);
 
@@ -13,27 +17,22 @@
 		getAuth()
 		user = authStore.user
 })
-
 </script>
-
+<App>
 <Header/>
-<div>
-{#if user?.role === "admin"}
- <AdminDashBoard/>
- {:else if user?.role === "instructor"}
- <InstructorDashBoard/>
- {:else if user?.role === "student"}
- <StudentDashBoard/>
-{/if}
-
-</div>
-
+<Main>
+        {#if user?.role === "admin"}
+        <AdminDashBoard/>
+        {:else if user?.role === "instructor"}
+        <InstructorDashBoard/>
+        {:else if user?.role === "student"}
+        <StudentDashBoard/>
+        {/if}
+</Main>
 <Footer/>
+</App>
+
+
 
 <style>
-div{
-        display: flex;
-        justify-content:  center;
-
-}
 </style>
