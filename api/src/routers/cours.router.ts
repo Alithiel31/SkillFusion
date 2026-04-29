24166ce579ce19f985f4485373f76c1491c2d390
Alithiel31 <harmo.man.01@gmail.com>
2026-04-29 04:49:51 -0400

    RGPD routes added on cours.router.ts

--- api/src/routers/cours.router.ts
+++ api/src/routers/cours.router.ts
@@ -1,13 +1,18 @@
 import express from 'express';
 import coursController from '../controllers/cours.controller';
+import { verifyToken } from '../middlewares/auth.middleware';
 
-const router= express.Router();
+const router = express.Router();
 
-router.get("/cours",coursController.getAll)
-router.get("/cours/homepage",coursController.getForHomePage)
-router.get("/cours/:id",coursController.getOneCours)
-router.post("/cours",coursController.createCours)
-router.patch("/cours/:id",coursController.updatingCours)
-router.delete("/cours/:id",coursController.deleteCours)
+router.get("/cours", coursController.getAll)
+router.get("/cours/homepage", coursController.getForHomePage)
+router.get("/cours/:id", coursController.getOneCours)
+router.post("/cours", coursController.createCours)
+router.patch("/cours/:id", coursController.updatingCours)
+router.delete("/cours/:id", coursController.deleteCours)
+
+//Routes RGPD : suppression de compte
+router.patch("/cours/mine/transfer", verifyToken, coursController.transferMyCoursToAdmin)
+router.delete("/cours/mine", verifyToken, coursController.deleteAllMyCours)
 
 export default router;
