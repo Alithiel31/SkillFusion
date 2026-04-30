2cb3e6c362cdf73271fd4584226ce701b99666a1
Alithiel31 <harmo.man.01@gmail.com>
2026-04-30 05:49:20 -0400

    first batch of addinf rbac to routers

--- api/src/routers/badge.router.ts
+++ api/src/routers/badge.router.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import badgeController from '../controllers/badge.controller';
+import { verifyToken } from '../middlewares/auth.middleware';
+import { checkRoles, requireSelfOrAdmin, ROLES } from '../middlewares/rbac.middleware';
+
 
 const router = express.Router();
 
 router.get("/badges", badgeController.getAll)
 router.get("/badges/:id", badgeController.getOneBadge)
 
-router.post("/badges", badgeController.createBadge)
+router.post("/badges", verifyToken, checkRoles([ROLES.STUDENT, ROLES.TEACHER, ROLES.ADMIN]), badgeController.createBadge)
 
-router.patch("/badges/:id", badgeController.updatingBadge)
+router.patch("/badges/:id", verifyToken, requireSelfOrAdmin, badgeController.updatingBadge)
 
-router.delete("/badges/:id", badgeController.deleteBadge)
+router.delete("/badges/:id", verifyToken, requireSelfOrAdmin, badgeController.deleteBadge)
 
 export default router;
--- api/src/routers/cours.router.ts
+++ api/src/routers/cours.router.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import coursController from '../controllers/cours.controller';
 import { verifyToken } from '../middlewares/auth.middleware';
+import { checkRoles, requireSelfOrAdmin, ROLES } from '../middlewares/rbac.middleware';
 
 const router= express.Router();
 
@@ -8,8 +9,9 @@ router.get("/cours",coursController.getAll)
 router.get("/cours/homepage",coursController.getForHomePage)
 router.get("/cours/instructor/:id",coursController.getCoursByInstructor)
 router.get("/cours/:id",coursController.getOneCours)
-router.post("/cours",coursController.createCours)
-router.patch("/cours/:id",coursController.updatingCours)
-router.delete("/cours/:id", verifyToken, coursController.deleteCours)
+
+router.post("/cours", verifyToken, checkRoles([ROLES.TEACHER, ROLES.ADMIN]), coursController.createCours)
+router.patch("/cours/:id", verifyToken, requireSelfOrAdmin, coursController.updatingCours)
+router.delete("/cours/:id", verifyToken, requireSelfOrAdmin, coursController.deleteCours)
 
 export default router;
