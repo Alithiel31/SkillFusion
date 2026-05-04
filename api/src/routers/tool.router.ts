dc31fc0141ef402d163c1ae119a1a0a6a1a22c69
Alithiel31 <harmo.man.01@gmail.com>
2026-05-03 22:19:10 -0400

    add rbac to the tool.router.ts missing

--- api/src/routers/tool.router.ts
+++ api/src/routers/tool.router.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import toolController from '../controllers/tool.controller';
+import { verifyToken } from '../middlewares/auth.middleware';
+import { checkRoles, ROLES } from '../middlewares/rbac.middleware';
 
 const router = express.Router();
 
+// Routes publiques
 router.get("/tools", toolController.getAll);
 router.get("/tools/:id", toolController.getOneTool);
 
-router.post("/tools", toolController.createTool);
-
-router.patch("/tools/:id", toolController.updatingTool);
-
-router.delete("/tools/:id", toolController.deleteTool);
+// Routes réservées à l'admin
+router.post("/tools", verifyToken, checkRoles([ROLES.ADMIN]), toolController.createTool);
+router.patch("/tools/:id", verifyToken, checkRoles([ROLES.ADMIN]), toolController.updatingTool);
+router.delete("/tools/:id", verifyToken, checkRoles([ROLES.ADMIN]), toolController.deleteTool);
 
 export default router;
